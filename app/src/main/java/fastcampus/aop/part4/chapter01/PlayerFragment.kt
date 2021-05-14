cc9e018f70246ff0c93e5b6cd2f359b537625412
kimminkyu <kmk50009@naver.com>
2021-05-15 00:43:42 +0900

    YouTube MotionLayout 사용하여 UI 구성

---
+++ app/src/main/java/fastcampus/aop/part4/chapter01/PlayerFragment.kt
@@ -0,0 +1,46 @@
+package fastcampus.aop.part4.chapter01
+
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.fragment.app.Fragment
+import fastcampus.aop.part4.chapter01.databinding.FragmentPlayerBinding
+import kotlin.math.abs
+
+class PlayerFragment: Fragment(R.layout.fragment_player) {
+
+    private var binding: FragmentPlayerBinding? = null
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
+        binding = fragmentPlayerBinding
+
+        fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                binding?.let {
+                    (activity as MainActivity).also { mainActivity ->
+                        mainActivity.findViewById<MotionLayout>(R.id.mainMotionLayout).progress = abs(progress)
+
+                    }
+                }
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {}
+
+        })
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
